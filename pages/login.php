c54dad444c8314c28ff29a44bc8b1a52c1bc53b1
or-abdillh <hans.abdillh05@gmail.com>
2021-12-10 15:01:15 +0700

    Create login page

--- pages/login.php
+++ pages/login.php
@@ -5,9 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/login.css">
+
     <title>Login - Stock Inventory Management</title>
 </head>
 <body>
-    <div class="app"></div>
+    <section class="login-wrapper">
+        <div class="header">
+            <p>Login</p>
+        </div>
+        <div class="content">
+            <form action="">
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" placeholder="Input your username here">
+                </div>
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" placeholder="Input your username here">
+                </div>
+                <div class="form-input">
+                    <button type="submit" class="btn-submit">Login</button>
+                </div>
+            </form>
+            <p class="text-footer">
+                &copy; 2021 Oka Rajeb Abdillah
+            </p>
+        </div>
+
+    </section>
 </body>
 </html>
