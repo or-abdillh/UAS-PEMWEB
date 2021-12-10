0c07bc466429c7946253de41e54426fa543f7456
or-abdillh <hans.abdillh05@gmail.com>
2021-12-10 13:36:57 +0700

    Create connection

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+    require("./helper/connection.php");
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock - Inventory Management System</title>
+</head>
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
